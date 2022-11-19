ebc1a7a9ead6407dd3d1445927417fa2afe23a8f
dstx7 <edinson.florezx7@gmail.com>
2022-11-18 22:22:18 -0500

    conexiones tabla empleados

--- empleados_front/src/components/empleados/empleados.buscar.js
+++ empleados_front/src/components/empleados/empleados.buscar.js
@@ -1,89 +1,38 @@
 import React from "react";
 import { Container, Row } from "react-bootstrap";
 import { request } from "../helper/helper";
-import "./empleados.buscar.css";
-import BootstrapTable from "react-bootstrap-table-next";
-import paginationFactory, {
-  PaginationProvider,
-  PaginationListStandalone,
-} from "react-bootstrap-table2-paginator";
-import { Route } from "react-router-dom";
-
-const products = [
-  {
-    id: 1,
-    name: "Turbo",
-    price: 500,
-  },
-  {
-    id: 2,
-    name: "Header",
-    price: 600,
-  },
-  {
-    id: 3,
-    name: "Bodykit",
-    price: 800,
-  },
-  {
-    id: 4,
-    name: "Turbo",
-    price: 500,
-  },
-  {
-    id: 5,
-    name: "suspension neumatica",
-    price: 900,
-  },
-  {
-    id: 6,
-    name: "Bodykit",
-    price: 800,
-  },
-  {
-    id: 7,
-    name: "Turbo",
-    price: 500,
-  },
-  {
-    id: 8,
-    name: "Header",
-    price: 600,
-  },
-  {
-    id: 9,
-    name: "Bodykit",
-    price: 800,
-  },
-  {
-    id: 10,
-    name: "Frenos ceramicos",
-    price: 500,
-  },
-  {
-    id: 11,
-    name: "Header",
-    price: 600,
-  },
-  {
-    id: 12,
-    name: "Bodykit",
-    price: 800,
-  },
-];
+import "./empleados.css";
+import DataGrid from "../grid/grid";
 
 const columns = [
   {
-    dataField: "id",
-    text: "Product ID",
+    dataField: "_id",
+    text: "ID",
+    hidden: true,
   },
   {
-    dataField: "name",
-    text: "Product Name",
+    dataField: "nombre",
+    text: "Nombre",
   },
   {
-    dataField: "price",
-    text: "Product Price",
+    dataField: "apellido_p",
+    text: "Primer Apellido",
+  },
+  {
+    dataField: "apellido_m",
+    text: "Segundo Apellido",
+  },
+  {
+    dataField: "telefono",
+    text: " Telefono",
+  },
+  {
+    dataField: "mail",
+    text: "Correo Electronico",
+  },
+  {
+    dataField: "direccion",
+    text: " Direccion",
   },
 ];
 
@@ -92,43 +41,26 @@ export default class EmpleadosBuscar extends React.Component {
     super(props);
     this.state = {};
   }
-
   componentDidMount() {
     request
-      .get("/empleados")
+      .get(this.props.url)
       .then((response) => {
-        console.log(response.data);
+        this.setState({ rows: response.data });
       })
-      .catch((err) => {
-        console.log(err);
+      .catch((error) => {
+        console.log(error);
       });
   }
   render() {
-    const options = {
-      custom: true,
-      totalSize: products.length,
-    };
-
+  
     return (
       <Container id="empleados-buscar-container">
         <Row>
-          <h2>PRODUCTS</h2>
+          <h2>EMPLEADOS</h2>
         </Row>
-        <Route>
-          <PaginationProvider pagination={paginationFactory(options)}>
-            {({ paginationProps, paginationTableProps }) => (
-              <div>
-                <PaginationListStandalone {...paginationProps} />
-                <BootstrapTable
-                  keyField="id"
-                  data={products}
-                  columns={columns}
-                  {...paginationTableProps}
-                />
-              </div>
-            )}
-          </PaginationProvider>
-        </Route>
+        <Row>
+        <DataGrid url="/empleados" columns={ columns }/>
+        </Row>
       </Container>
     );
   }
